2c61bf9f79fc33385887f4af85ee0c2925a574e0
samuelmarina <samuellmarina@gmail.com>
2021-08-17 16:01:14 -0400

    Initialize package

---
+++ index.js
@@ -0,0 +1,14 @@
+function isOdd(number){
+    if(number === 1) return true;
+    if(number === 2) return false;
+    if(number === 3) return true;
+    if(number === 4) return false;
+    if(number === 5) return true;
+    if(number === 6) return false;
+    if(number === 7) return true;
+    if(number === 8) return false;
+    if(number === 9) return true;
+    if(number === 10) return false;
+}
+
+module.exports = isOdd;
