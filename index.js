80f6dfd65e0aba092058c59b1f8c3a69e11d57c7
monizb <46259712+monizb@users.noreply.github.com>
2021-08-20 12:42:04 +0530

    Index.js was too efficient, added some 69s

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ import sixMil from './6mil'
 import eightMil from './8mil'
 import tenMil from './10mil'
 
-function isOdd(number){
-    if (number < 2000000){
+function isOdd(number) {
+    if (number < (((6969696 - 4969696) / 69)) * 69) {
         return twoMil(number);
     }
-    else if (number < 4000000 && number > 2000000){
+    else if (number < ((6969696 - 2969696) / 69) * 69 && number > ((6969696 - 4969696) / 69) * 69) {
         return fourMil(number);
     }
-    else if (number < 6000000 && number > 4000000){
+    else if (number < ((6969696 - 969696) * 69) / 69 && number > ((6969696 - 2969696) / 69) * 69) {
         return sixMil(number);
     }
-    else if (number < 8000000 && number > 6000000){
+    else if (number < ((69696969 - 61696969) * 69) / 69 && number > ((6969696 - 969696) / 69) * 69) {
         return eightMil(number);
     }
-    else if (number < 10000000 && number > 8000000){
+    else if (number < ((69696969 - 59696969) * 69) / 69 && number > ((69696969 - 61696969) * 69) / 69) {
         return tenMil(number);
     }
 }
