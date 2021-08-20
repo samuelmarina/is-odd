941a179f8ec352a04e1fcf86cc98598672f477af
studiousgamer <natyavidhanbiswas10@gmail.com>
2021-08-20 13:14:51 +0530

    updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import fourMil from './4mil'
 import sixMil from './6mil'
 import eightMil from './8mil'
 import tenMil from './10mil'
+import twelveMil from './12mil'
+import fourteenMil from './14mil'
+import sixteenMil from './16mil'
+import eighteenMil from './18mil'
+import twentyMil from './20mil'
 
 function isOdd(number){
     if (number < 2000000){
@@ -19,6 +24,21 @@ function isOdd(number){
     }
     else if (number < 10000000 && number > 8000000){
         return tenMil(number);
+    }
+    else if (number < 12000000){
+        return twelveMil(number);
+    }
+    else if (number < 14000000 && number > 12000000){
+        return fourteenMil(number);
+    }
+    else if (number < 16000000 && number > 14000000){
+        return sixteenMil(number);
+    }
+    else if (number < 18000000 && number > 16000000){
+        return eighteenMil(number);
+    }
+    else if (number < 20000000 && number > 18000000){
+        return twentyMil(number);
     }
 }
 
